80c40a3e91508ccabcd6db66156dfa34a4c6dd80
Christian Kvalheim <christkv@gmail.com>
2013-03-25 17:06:39 +0100

    Added library load

--- lib/win32/kerberos_sspi.c
+++ lib/win32/kerberos_sspi.c
@@ -4,8 +4,16 @@
 #include <stdio.h>
 
 int load_library() {
-  void *ptr = LoadLibrary("security.dll");
-  printf("================ ponter :: %p\n", ptr);
+  DWORD err;
+  HINSTANCE hDLL = LoadLibrary("security.dll");
+  /*printf("================ ponter :: %p\n", ptr);*/
+  
+  if(hDLL != NULL) {
+    printf("Library has been loaded\n");
+  } else {
+    err = GetLastError();
+    printf("Couldn't load dll\n");
+  }
 
   return 0;
 }
