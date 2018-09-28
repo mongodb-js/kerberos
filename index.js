5be618fbadac269ae0e8cd4ef4fcc78f98ada151
Jorge Bay Gondra <jorgebaygondra@gmail.com>
2018-09-28 22:14:27 +0200

    feat(package): export the package version
    As kerberos can be used as an optional dependency, we expose the
    version at api level to allow third party libraries to consume the
    version number easily.
    
    It includes a test that verifies what's being exported.

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ module.exports = kerberos;
 // Support legacy versions of the mongodb driver which expect this export
 module.exports.Kerberos = kerberos;
 
+module.exports.version = require('./package.json').version;
+
 // Set up the auth processes
 module.exports.processes = {
   MongoAuthProcess: require('./lib/auth_processes/mongodb').MongoAuthProcess
