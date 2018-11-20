b53a3ea4e07517d86c57d131fd476be2c5fecc55
Matt Broadstone <mbroadst@gmail.com>
2018-11-20 13:53:30 -0500

    test(define-operation): improve coverage of `defineOperation`

--- test/defineOperation_tests.js
+++ test/defineOperation_tests.js
@@ -6,6 +6,7 @@ const expect = require('chai').expect;
 const testMethod = defineOperation(kerberos._testMethod, [
   { name: 'string', type: 'string' },
   { name: 'shouldError', type: 'boolean', default: false },
+  { name: 'optionalString', type: 'string', required: false },
   { name: 'callback', type: 'function', required: false }
 ]);
 
@@ -14,8 +15,30 @@ describe('defineOperation', () => {
     expect(() => testMethod(42)).to.throw(/Invalid type for parameter/);
   });
 
+  it('should support defaults', function(done) {
+    expect(() => testMethod('testing')).to.not.throw();
+    testMethod('testing', true, err => {
+      expect(err).to.exist;
+      done();
+    });
+  });
+
   it('should return a promise if no callback is provided', function() {
     const promise = testMethod('llamas', false);
     expect(promise).to.be.instanceOf(Promise);
+  });
+
+  it('should use a callback if provided', function(done) {
+    testMethod('testing', false, 'optional', (err, result) => {
+      expect(err).to.not.exist;
+      expect(result).to.equal('optional');
+
+      testMethod('testing', true, 'optional', (err, result) => {
+        expect(err).to.exist;
+        expect(result).to.not.exist;
+
+        done();
+      });
+    });
   });
 });
