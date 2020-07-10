36574dc56b4a600bde455a7851fcba477b7c81ee
emadum <eric.adum@mongodb.com>
2020-07-10 17:51:20 -0400

    add run-tests task, clean up extra buildvariants

---
+++ .evergreen/run-tests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -o errexit  # Exit the script with error if any of the commands fail
+
+NODE_ARTIFACTS_PATH="${PROJECT_DIRECTORY}/node-artifacts"
+
+if [ "$OS" == "Windows_NT" ]; then
+    export NVM_HOME=`cygpath -w "$NODE_ARTIFACTS_PATH/nvm"`
+    export NVM_SYMLINK=`cygpath -w "$NODE_ARTIFACTS_PATH/bin"`
+    export PATH=`cygpath $NVM_SYMLINK`:`cygpath $NVM_HOME`:$PATH
+else
+    export PATH="/opt/mongodbtoolchain/v2/bin:$PATH"
+    export NVM_DIR="${NODE_ARTIFACTS_PATH}/nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+fi
+
+npm test
