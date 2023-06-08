a16a7ee52c204300822dc9ec122055858de680e7
Bailey Pearson <bailey.pearson@gmail.com>
2023-06-08 11:56:11 -0600

    ci(NODE-5332): download npm to node_artifacts directory in CI (#156)
    Co-authored-by: Neal Beeken <neal.beeken@mongodb.com>

--- .evergreen/run-prebuild.sh
+++ .evergreen/run-prebuild.sh
@@ -3,7 +3,10 @@
 set -o errexit  # Exit the script with error if any of the commands fail
 set -o xtrace
 
-source "${PROJECT_DIRECTORY}/.evergreen/init-nvm.sh"
+source "${PROJECT_DIRECTORY}/.evergreen/init-node-and-npm-env.sh"
+
+# mongodbtoolchain is necessary for building on Windows
+export PATH="/opt/mongodbtoolchain/v2/bin:$PATH"
 
 echo "Node Version $(node -v)"
 
