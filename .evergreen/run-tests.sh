9becdc8d02dac9276313fd6ae2f0b444a4b72c02
Neal Beeken <neal.beeken@mongodb.com>
2024-09-27 13:38:32 -0400

    chore: driver_tools init

--- .evergreen/run-tests.sh
+++ .evergreen/run-tests.sh
@@ -3,7 +3,7 @@
 set -o errexit  # Exit the script with error if any of the commands fail
 set -o xtrace
 
-source ./.drivers-tools/.evergreen/init-node-and-npm-env.sh
+source $DRIVERS_TOOLS/.evergreen/init-node-and-npm-env.sh
 
 npm run check:lint
 npm test
