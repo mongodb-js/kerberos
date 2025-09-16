3c5d5533f2a1eb4e715b9d949d175120265ecafd
bailey <bailey.pearson@mongodb.com>
2025-09-16 15:33:20 -0600

    add linting for d.ts file

--- index.d.ts
+++ index.d.ts
@@ -16,110 +16,110 @@ export const GSS_MECH_OID_SPNEGO: number;
  * Optional settings for *KerberosClient.wrap* method.
  */
 export interface WrapOptions {
-    /**
-     * The user to authorize.
-     */
-    user?: string | undefined;
+  /**
+   * The user to authorize.
+   */
+  user?: string;
 
-    /**
-     * Indicates if the wrap should request message confidentiality.
-     */
-    protect?: boolean
+  /**
+   * Indicates if the wrap should request message confidentiality.
+   */
+  protect?: boolean;
 }
 
 /**
  * Options for `initializeClient()`.
  */
 export interface InitializeClientOptions {
-    /**
-     * Optional string containing the client principal in the form 'user@realm' (e.g. 'jdoe@example.com').
-     */
-    principal?: string | undefined;
-    /**
-     * Optional integer used to set GSS flags. (e.g.  `GSS_C_DELEG_FLAG|GSS_C_MUTUAL_FLAG|GSS_C_SEQUENCE_FLAG` will allow for forwarding credentials to the remote host).
-     */
-    gssFlag?: number | undefined;
-    /**
-     * Optional GSS mech OID. Defaults to None (GSS_C_NO_OID). Other possible values are `GSS_MECH_OID_KRB5`, `GSS_MECH_OID_SPNEGO`.
-     */
-    mechOID?: number | undefined;
+  /**
+   * Optional string containing the client principal in the form 'user@realm' (e.g. 'jdoe@example.com').
+   */
+  principal?: string;
+  /**
+   * Optional integer used to set GSS flags. (e.g.  `GSS_C_DELEG_FLAG|GSS_C_MUTUAL_FLAG|GSS_C_SEQUENCE_FLAG` will allow for forwarding credentials to the remote host).
+   */
+  gssFlag?: number;
+  /**
+   * Optional GSS mech OID. Defaults to None (GSS_C_NO_OID). Other possible values are `GSS_MECH_OID_KRB5`, `GSS_MECH_OID_SPNEGO`.
+   */
+  mechOID?: number;
 
-    /**
-     * The username with which to authenticate.  Only used on Windows.
-     */
-    user?: string;
-    
-    /**
-     * The password with which to authenticate.  Only used on Windows.
-     */
-    pass?: string;
+  /**
+   * The username with which to authenticate.  Only used on Windows.
+   */
+  user?: string;
+
+  /**
+   * The password with which to authenticate.  Only used on Windows.
+   */
+  pass?: string;
 }
 
 export class KerberosClient {
-    /**
-     * The username used for authentication.
-     */
-    username: string;
-    /**
-     * The last response received during authentication steps.
-     */
-    response: string;
-    /**
-     * Indicates whether confidentiality was applied or not (GSSAPI only).
-     */
-    responseConf: string;
-    /**
-     * Indicates that authentication has successfully completed or not.
-     */
-    contextComplete: boolean;
+  /**
+   * The username used for authentication.
+   */
+  username: string;
+  /**
+   * The last response received during authentication steps.
+   */
+  response: string;
+  /**
+   * Indicates whether confidentiality was applied or not (GSSAPI only).
+   */
+  responseConf: string;
+  /**
+   * Indicates that authentication has successfully completed or not.
+   */
+  contextComplete: boolean;
 
-    /**
-     * Processes a single kerberos client-side step using the supplied server challenge.
-     *
-     * @param challenge A string containing the base64-encoded server data (which may be empty for the first step)
-     */
-    step(challenge: string): Promise<string>;
+  /**
+   * Processes a single kerberos client-side step using the supplied server challenge.
+   *
+   * @param challenge A string containing the base64-encoded server data (which may be empty for the first step)
+   */
+  step(challenge: string): Promise<string>;
 
-    /**
-     * Perform the client side kerberos wrap step.
-     *
-     * @param challenge The response returned after calling `unwrap`
-     * @param options Optional settings
-     */
-    wrap(challenge: string, options?: WrapOptions): Promise<string>;
+  /**
+   * Perform the client side kerberos wrap step.
+   *
+   * @param challenge The response returned after calling `unwrap`
+   * @param options Optional settings
+   */
+  wrap(challenge: string, options?: WrapOptions): Promise<string>;
 
-    /**
-     * Perform the client side kerberos unwrap step
-     *
-     * @param challenge A string containing the base64-encoded server data
-     */
-    unwrap(challenge: string): Promise<string>;
+  /**
+   * Perform the client side kerberos unwrap step
+   *
+   * @param challenge A string containing the base64-encoded server data
+   */
+  unwrap(challenge: string): Promise<string>;
 }
 
 export class KerberosServer {
-    /**
-     * The username used for authentication
-     */
-    username: string;
-    /**
-     * @description The last response received during authentication steps
-     */
-    response: string;
-    /**
-     * @description The target used for authentication
-     */
-    targetName: string;
-    /**
-     * @description Indicates that authentication has successfully completed or not
-     */
-    contextComplete: boolean;
+  /**
+   * The username used for authentication
+   */
+  username: string;
+  /**
+   * @description The last response received during authentication steps
+   */
+  response: string;
+  /**
+   * @description The target used for authentication
+   */
+  targetName: string;
+  /**
+   * @description Indicates that authentication has successfully completed or not
+   */
+  contextComplete: boolean;
 
-    /**
-     * Processes a single kerberos server-side step using the supplied client data.
-     *
-     * @param challenge A string containing the base64-encoded client data
-     */
-    step(challenge: string): Promise<string>;
+  /**
+   * Processes a single kerberos server-side step using the supplied client data.
+   *
+   * @param challenge A string containing the base64-encoded client data
+   */
+  step(challenge: string): Promise<string>;
 }
 
 /**
@@ -144,7 +144,12 @@ export class KerberosServer {
  * @param service The Kerberos service to check access for.
  * @param defaultRealm The default realm to use if one is not supplied in the user argument.
  */
-export function checkPassword(name: string, password: string, service: string, defaultRealm?: string): Promise<void>;
+export function checkPassword(
+  name: string,
+  password: string,
+  service: string,
+  defaultRealm?: string
+): Promise<void>;
 
 /**
  * This function returns the service principal for the server given a service type and hostname.
@@ -162,7 +167,10 @@ export function principalDetails(service: string, hostname: string): Promise<str
  * @param service A string containing the service principal in the form '`type@fqdn`'.
  * @param [options] Optional settings
  */
-export function initializeClient(service: string, options?: InitializeClientOptions): Promise<KerberosClient>;
+export function initializeClient(
+  service: string,
+  options?: InitializeClientOptions
+): Promise<KerberosClient>;
 
 /**
  * Initializes a context for server-side authentication with the given service principal.
